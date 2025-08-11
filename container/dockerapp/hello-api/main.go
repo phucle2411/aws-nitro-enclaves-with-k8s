b0c43ecb87c0bd192de6ce247343a19535884c78
phucle <phuc.uit.nt06@gmail.com>
2025-08-11 22:11:23 +0700

    up

--- container/dockerapp/hello-api/main.go
+++ container/dockerapp/hello-api/main.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
+func printHelloPeriodically() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Println("hello from enclave")
+		}
+	}
+}
+
 func main() {
+	// Start the periodic hello message in a goroutine
+	go printHelloPeriodically()
+
 	// Define the route and handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
