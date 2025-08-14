6c383ded40b0bb0a6fca1f43e362d46792a9c806
phucle <phuc.uit.nt06@gmail.com>
2025-08-15 00:55:27 +0700

    up

--- container/hello-api/run.sh
+++ container/hello-api/run.sh
@@ -10,7 +10,7 @@ main() {
         echo "AWS_REGION must be set" >&2
         exit 1
     fi
-    vsock-proxy 8000 kms.ap-southeast-1.amazonaws.com 443 &
+    #vsock-proxy 8000 kms.ap-southeast-1.amazonaws.com 443 &
 
     nitro-cli run-enclave --cpu-count $ENCLAVE_CPU_COUNT --memory $ENCLAVE_MEMORY_SIZE \
         --eif-path $EIF_PATH --debug-mode
@@ -20,15 +20,15 @@ main() {
     echo "Enclave ID is $enclave_id"
     echo "-------------------------------"
 
-    # Start the proxy in background
-    # echo "Starting VSOCK proxy..."
-    # python3 $PROXY_SCRIPT both --config /home/proxy-config.json \
-    #     --log-level INFO > /var/log/vsock-proxy.log 2>&1 &
-    # PROXY_PID=$!
+    Start the proxy in background
+    echo "Starting VSOCK proxy..."
+    python3 $PROXY_SCRIPT both --config /home/proxy-config.json \
+        --log-level INFO > /var/log/vsock-proxy.log 2>&1 &
+    PROXY_PID=$!
     
-    # echo "Proxy started with PID: $PROXY_PID"
-    # echo "Proxy logs: /var/log/vsock-proxy.log"
-    # echo "-------------------------------"
+    echo "Proxy started with PID: $PROXY_PID"
+    echo "Proxy logs: /var/log/vsock-proxy.log"
+    echo "-------------------------------"
     
     nitro-cli console --enclave-id $enclave_id # blocking call.
 }
